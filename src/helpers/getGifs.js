df80d920c5c21ef104595747b9aae14497a577e1
Sergio Cano <canosergio90@gmail.com>
2020-12-07 21:38:06 -0300

    Custom hook (#3)

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,22 +1,24 @@
 // TODO externalize property
-const gitUrl = "http://api.giphy.com/v1/gifs"
+const gitUrl = 'http://api.giphy.com/v1/gifs'
 
 // TODO externalize property
 const apiKey = 'someApiKey'
 
-export const getGifs = async( category ) => {
-    // TODO refactor hardcoded url query params
-    const url = `${gitUrl}/search?q=${encodeURI(category)}&limit=10&api_key=${apiKey}`
-    const resp = await fetch( url )
-    const { data } = await resp.json()
+export const getGifs = async category => {
+  // TODO refactor hardcoded url query params
+  const url = `${gitUrl}/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=${apiKey}`
+  const resp = await fetch(url)
+  const { data } = await resp.json()
 
-    const gifs = data.map(img => {
-        return {
-            id: img.id,
-            title: img.title,
-            url: img.images?.downsized_medium.url
-        }
-    })
-    
-    return gifs
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url
+    }
+  })
+
+  return gifs
 }
